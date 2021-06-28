186cbbec2c2fb710a9e071c63a35eba1c6439089
asekito <dresekito@gmail.com>
2021-06-28 00:43:43 -0700

    cardio vs strength display on modal

--- client/src/CalendarPage/CalendarEventModal.tsx
+++ client/src/CalendarPage/CalendarEventModal.tsx
@@ -17,7 +17,27 @@ export const CalendarEventModal = ({
     <Modal open={eventModal} onClose={() => setEventModal(false)}>
       <div className="medium-modal-white">
         {exercises.map((e) => (
-          <div>{e.exercise}</div>
+          <>
+            {e.type === "cardio" ? (
+              <div>
+                <div>{e.exercise}</div>
+                <div>Total time: {e.time} Min</div>
+                <div>
+                  Total distance: {e.distance} {e.distanceUnit}
+                </div>
+              </div>
+            ) : null}
+            {e.type === "strength" ? (
+              <div>
+                <div>{e.exercise}</div>
+                <div>Sets: {e.sets}</div>
+                <div>Reps: {e.reps}</div>
+                <div>
+                  Weight: {e.weight} {e.weightUnit}
+                </div>
+              </div>
+            ) : null}
+          </>
         ))}
       </div>
     </Modal>
